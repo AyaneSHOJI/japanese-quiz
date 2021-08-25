09456b64ed4612c8bea46c4600f6f92515c385f6
Ayane <shojiayane@gmail.com>
2021-08-25 23:45:48 +0200

    Add project files.

---
+++ ClassLibrary1/Buisness.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary1
+{
+    public class Buisness
+    {
+        public string CalculBusiness(int i)
+        {
+            return $"La réponse est {i*2}";
+        }
+    }
+
+    public class Question
+    {
+        public List<string> elements;
+
+        public override string ToString()
+        {
+            string result = $"{elements[0]} <br />";
+            for (int i = 1; i < elements.Count - 1; i++)
+                result += $"{i}. {elements[i]}<br />";
+            return result;
+        }
+
+
+    }
+}
